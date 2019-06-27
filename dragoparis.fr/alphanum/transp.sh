8a4c4c7c1486d870413371ac0ca8d284bc3d2f24
Yannick Battail <yannick.battail@raisepartner.com>
2019-06-27 18:42:13 +0200

    change transparent background method

--- dragoparis.fr/alphanum/transp.sh
+++ dragoparis.fr/alphanum/transp.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+
+#rm *.png
+
 for PHOTO in *.jpg
 do
     BASE=`basename $PHOTO .jpg`
-    magick convert "$PHOTO" -fuzz 2% -transparent white -trim "$BASE.png"
+    #magick convert "$PHOTO" -fuzz 2% -transparent white -trim "$BASE.png"
+    magick convert "$PHOTO" -fuzz 2% -fill "rgba(0,0,0,0)" -draw 'color 0,0 floodfill' -trim "$BASE.png"
+    echo "$BASE.png"
 done
