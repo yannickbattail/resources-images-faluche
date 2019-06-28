58ccccd976c22d10aca5140d2a6910f8ef613ccf
Yannick Battail <yannick.battail@raisepartner.com>
2019-06-28 19:06:20 +0200

    renommage paris

--- folygraphie.com/scripts/mass_rename.sh
+++ folygraphie.com/scripts/mass_rename.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for i in *.jpg
 do
-    mv "$i" $(echo $i | sed 's/-/_/' | tr [:upper:] [:lower:] )
+    mv "$i" $(echo $i | sed 's/-/_/' | sed 's/fip[0-9][0-9]_//'  | sed 's/_small//' | tr [:upper:] [:lower:] )
 done
